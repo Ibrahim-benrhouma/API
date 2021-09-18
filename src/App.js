71a0ce21898da7128ec3354e4009df0c1e3b711d
Ibrahim-benrhouma <ibrahimbenrhouma9@gmail.com>
2021-09-18 17:24:35 +0100

    API

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import UserList from './components/UserList';
+import 'bootstrap/dist/css/bootstrap.css';
+
+function App() {
+  return (
+    <div className="container" >
+    <UserList/>
+    </div>
+  );
+}
+
+export default App;
